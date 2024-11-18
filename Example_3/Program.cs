4c7b47f38454f2fb380df0cac91106e77898c292
drmidn1ght <ana.drmidnight@gmail.com>
2024-11-18 19:04:30 -0300

    Creating 3rd example in Example_3\program.cs

--- Example_3/Program.cs
+++ Example_3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GoodPractices
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+            //Declaring variables 
+            string name = "Louis Tully";
+            int age = 31;
+            double income = 5000.00;
+
+            if (age >= 18)
+            {
+                Console.WriteLine($"{name}is not a minor. ");
+            }
+        }
+    }
+}
